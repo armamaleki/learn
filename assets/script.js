9a91ffde013542223347a480cea476c501d91b7a
arma <arma.malekii@gmail.com>
2023-08-10 12:36:43 +0330

    first commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// var openMenuInMobile = document.getElementById('openMenuInMobile');
+var adminMenu = document.getElementById('adminMenu');
+// openMenuInMobile.addEventListener("click" , function () {
+//     adminMenu.style.display='flex'
+// })
+
+
+function openMenuBar() {
+    adminMenu.style.display='flex'
+}
